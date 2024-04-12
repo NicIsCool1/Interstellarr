bec328a13b3f21b8b003267df26b2e1151590709
NicIsCool1 <164079439+NicIsCool1@users.noreply.github.com>
2024-04-11 17:20:21 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, 
   users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+     NicLuhtwizzy: 'unblockedproxy123',
   },
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
+  routes: true, 
+  local: true, 
 }
 export default config
